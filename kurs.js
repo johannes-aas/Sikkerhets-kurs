c7d3e564d05adfa4767a5535c2b9553028f9e0ef
Johannes Hansen Aas <johannes.hansen.aas@gmail.com>
2020-10-21 02:02:02 +0200

    created json object for course

---
+++ kurs.js
@@ -0,0 +1,30 @@
+var kurs = {'kurs' : [
+    {
+        'navn' : 'sikkerhet1',
+        'kapittel' : [
+            {
+                'nr' : '1',
+                'navn' : 'kapittel 1',
+                'slides' : [
+                    {
+                        'type' : 'video',
+                        'innhold' : '<p>stuff</p>'
+                    },
+                    {
+                        'type' : 'quiz',
+                        'Q&A' : [
+                            {
+                                'q' : '1+1???',
+                                'alternativ' : [
+                                    '1',
+                                    '2',
+                                    '3'
+                                ]
+                            }
+                        ] 
+                    }
+                ]
+            }
+        ]
+    }
+]};
